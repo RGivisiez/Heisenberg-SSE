cddedd770b4a82c7e620af95b6a3e2564e03cb47
RGivisiez <ronaldogivisiez@gmail.com>
2020-07-23 20:34:41 -0300

    Cubic lattice added.

--- README.md
+++ README.md
@@ -4,10 +4,7 @@ A program made to simulate a quantum Heisenberg model with spin 1/2 in 1D, 2D or
 
 The original program was made by Anders W. Sandvik [[1]](http://physics.bu.edu/~sandvik/programs/ssebasic/ssebasic.f90) for a lecture about [Quantum Monte Carlo Methods at Work for Novel Phases of Matter](http://physics.bu.edu/~sandvik/trieste12/index.html), presented in Trieste, Italy, Jan 23 - Feb 3, 2012. A more complete introduction about quantum spin systems and computational methods can be found [here](https://arxiv.org/abs/1101.3281).
 
-(Currently the 3D lattice is in development)
-
 ## How to use
-
 
 **Prerequisites:**
 
@@ -20,6 +17,10 @@ The original program was made by Anders W. Sandvik [[1]](http://physics.bu.edu/~
 gfortran heisenberg_sse.f90 -o heisenberg && ./heisenberg
 ```
 
+**Parameters:**
+
+- All the program parameters are in Module Variables.
+
 ## Running the tests
 
 In this [lecture](http://physics.bu.edu/~sandvik/trieste12/tut1.pdf) you will find simple tests to check the program correctness. 
@@ -31,6 +32,15 @@ For a 1D Heiserberg model with spin 1/2 and periodic boundary condition, the exa
 ![Ground_state_energy](Graphs/Ground_state_energy.png)
 
 Simulation made with a lattice size Lx=32, 10.000 thermalization steps, 10.000 Monte Carlo steps and 20 bins.
+
+**Others:**
+
+Evolution of the expansion cut-off:
+
+![Cut-off](Graphs/Cut-off_adjust.png)
+
+Distribution of the number of H-operators:
+![dist_n](Graphs/Prob_dist_n.png)
 
 ## License
 
--- heisenberg_sse.f90
+++ heisenberg_sse.f90
@@ -1,17 +1,40 @@
 Module Variables
 
-  Integer*4 :: lx, ly, lz, d, L
-  Integer*4 :: Nb, N, NH, NH_max = 0
-  Integer*4 :: Nbins, mcsteps, term_steps
+  ! Critical temperature for a 3D Heisenberg model is Tc = 0.945d0.
 
-  Real*8 :: beta, ener, n_opH = 0.0d0
+  ! lx=10, ly=1, lz=1 means a 1D chain lattice with size 10.
+  ! lx=10, ly=10, lz=1 means a 2D square lattice.
+  ! lx=10, ly=10, lz=10 means a 3D cubic lattice.
 
-  Integer*4, Allocatable, Dimension (:) :: spin
-  Integer*4, Allocatable, Dimension (:) :: opstring
-  Integer*4, Allocatable, Dimension (:,:) :: bound
-  Integer*4, Allocatable, Dimension (:) :: first_vertex_visitted
-  Integer*4, Allocatable, Dimension (:) :: last_vertex_visitted
-  Integer*4, Allocatable, Dimension (:) :: vertex_link
+  Integer*4, parameter :: lx = 10                     ! Number of spins in x.
+  Integer*4, parameter :: ly = 10                     ! Number of spins in y.
+  Integer*4, parameter :: lz = 10                     ! Number of spins in z.
+  Integer*4, parameter :: Nbins = 20                  ! Averages written to file after mcsteps.
+  Integer*4, parameter :: mcsteps = 1e5               ! Monte Carlo steps.
+  Integer*4, parameter :: term_steps = 1e5            ! Termalization steps.
+  Integer*4, parameter :: N = lx * ly * lz            ! Total number of spins.
+  Real*8,    parameter :: temp_ini = 1.4d0            ! Initial temperature.
+  Integer*4, parameter :: t_steps = 10                ! Number of temperature steps.
+  Real*8,    parameter :: dt = -0.1d0                 ! Size of temperature steps.
+
+  Integer*4 :: NH = 0               ! Number of H-operator.
+  Integer*4 :: L = max(4, N / 4)    ! Maximum string size. (Don't change it)
+  Integer*4 :: d                    ! Dimension. (Selected by the program)
+  Integer*4 :: Nb                   ! Number o bonds between spins. (N = d*lx*ly*lz)
+
+  Integer*4, Allocatable, Dimension (:)   :: spin                  ! Spin value.
+  Integer*4, Allocatable, Dimension (:)   :: opstring              ! Operator string ID.
+  Integer*4, Allocatable, Dimension (:,:) :: bound_spin_ID         ! List of the spin sites connect between the bound_spin_ID(1, bound_idx) and bound_spin_ID(2, bound_idx)
+  Integer*4, Allocatable, Dimension (:)   :: vertex_link           ! List of vertex links.
+  Integer*4, Allocatable, Dimension (:)   :: first_vertex_visitted ! First operator on each site in linked vertex list.
+  Integer*4, Allocatable, Dimension (:)   :: last_vertex_visitted  ! Last operator on each site in linked vertex list.
+
+  Integer*4 :: NH_max = 0
+  Character*60 :: arq
+
+  Real*8    :: beta
+  Real*8    :: n_opH = 0.0d0, n_opH2 = 0.0d0
+  Real*8    :: ususc = 0.0d0, staggered = 0.0d0
 
 End Module Variables
 
@@ -20,38 +43,48 @@ Program Main
   Use Variables
   Implicit None
 
-  Integer*4 seed(33), i, j
+  Integer*4 t  
 
-  seed(:33) = 42
+  write(arq, '("results_",I0,"x",I0,"x",I0,".dat")') lx, ly, lz
+  Open(40, file=arq, position='append')
 
-  Call random_seed(put=seed)
+  write(40, '(4x,"Beta",4x,"Energy",4x,"Specific Heat",4x,"Uniform Susceptibility", &
+           &  4x,"Staggered Magnetization",4x,"Number of H-operators",    &
+           &  4x,"(Number of H-operators)^2" )')
 
-  beta = 16.0d0  ! Inverse temperature.
+  Do t = 0, t_steps - 1
 
-  lx = 16 ! Number of spins in x.
-  ly = 16  ! Number of spins in y.
-  lz = 1  ! Number of spins in z.
+    beta = 1.0d0 / (temp_ini + dt * t)
 
-  NH = 0  ! Number of operator different of the Identity one. 
+    Allocate(opstring(0:L - 1))
+    Allocate(first_vertex_visitted(N), last_vertex_visitted(N))
+    Allocate(vertex_link(0:4 * L - 1))
 
-  N = lx * ly ! Total number of spins.
+    opstring = 0
 
-  L = max(4, N / 4)  ! Truncated system size.
+    Call lattice
+    Call init
 
-  Nbins = 20
+    Call termalization
 
-  term_steps = 1e4
+    Call qmc_steps
 
-  mcsteps = 1e4
+    Call results
 
-  Allocate(opstring(0:L-1))
-  opstring = 0
+    Call free_memory
 
-  Allocate(first_vertex_visitted(N), last_vertex_visitted(N))
-  Allocate(vertex_link(0:4 * L - 1))
+  end do
 
-  Call lattice
-  Call init
+  Close(40)
+
+End Program Main
+
+Subroutine termalization
+
+  Use Variables
+  Implicit None
+
+  Integer*4 i
 
   Do i = 1, term_steps
 
@@ -62,7 +95,18 @@ Program Main
 
   end do
 
-  ! Close(30)
+  write(*,'(1x,"Series cut-off: ",I0)') L
+  write(*,*)'----------------------------'
+  write(*,*)' '
+
+End Subroutine termalization
+
+Subroutine qmc_steps
+
+  Use Variables
+  Implicit None
+
+  Integer*4 i, j
 
   Do j = 1, Nbins
 
@@ -81,29 +125,30 @@ Program Main
 
   Close(10)
   Close(20)
+  Close(30)
 
-  Call results
-
-  Call free_memory
-
-End Program Main
+End Subroutine qmc_steps
 
 Subroutine lattice
 
   Use Variables
   Implicit None
 
-  Integer*4 :: i, j, bound_idx, spin_idx
+  Integer*4 :: i, j, k, bound_idx, spin_idx
 
-  ! mod(x, L) returns 0 if x == Lx or returns x+1 if x != Lx
+  ! mod(x, L), in this case, returns 0 if x == Lx or returns x if x != Lx.
 
-  if ( lx > 1 .and. ly > 1) then
+  write(*,*) " "
+  write(*,*)"heisenberg_sse.f90"
+  write(*,*) " "
 
-    print*, 'Sistema fechado 2D'
+  if ((lx > 1 .and. ly > 1) .and. (lz == 1)) then
 
-    d = 2                     ! System's dimension
-    Nb = d * lx*ly*lz         ! Number o bonds between spins.
-    Allocate(bound(2, Nb))
+    print*, '2D with Periodic boundary '
+
+    d = 2                     
+    Nb = d * lx*ly            
+    Allocate(bound_spin_ID(2, Nb))
 
     Do i = 0, lx - 1
       Do j = 0, ly - 1
@@ -111,31 +156,66 @@ Subroutine lattice
 
         bound_idx = 1 + i + j * lx
 
-        bound(1, bound_idx) = spin_idx
-        bound(2, bound_idx) = 1 + mod(i + 1, lx) + j * lx
+        bound_spin_ID(1, bound_idx) = spin_idx
+        bound_spin_ID(2, bound_idx) = 1 + mod(i + 1, lx) + j * lx
 
-        bound(1, bound_idx + N) = spin_idx
-        bound(2, bound_idx + N) = 1 + i + mod(j + 1, ly) * lx
+        bound_spin_ID(1, bound_idx + lx * ly) = spin_idx
+        bound_spin_ID(2, bound_idx + lx * ly) = 1 + i + mod(j + 1, ly) * lx
       end do
     end do
 
   elseif ( (lx > 1) .and. (ly == 1 .and. lz == 1) ) then
 
-    print*, 'Sistema fechado 1D'
+    print*, '1D with Periodic boundary '
 
-    d = 1                     ! System's dimension
-    Nb = d * lx*ly*lz         ! Number o bonds between spins.
-    Allocate(bound(2, Nb))
+    d = 1                     
+    Nb = d * lx               
+    Allocate(bound_spin_ID(2, Nb))
 
     Do i = 0, lx - 1
        spin_idx = i + 1
 
        bound_idx = i + 1
 
-       bound(1, bound_idx) = spin_idx
-       bound(2, bound_idx) = 1 + mod(i + 1, lx)
+       bound_spin_ID(1, bound_idx) = spin_idx
+       bound_spin_ID(2, bound_idx) = 1 + mod(i + 1, lx)
     end do
 
+  elseif (lx > 1 .and. ly > 1 .and. lz > 1) then
+
+    print*, '3D with Periodic boundary '
+
+    d = 3                     
+    Nb = d * lx * ly * lz
+    Allocate(bound_spin_ID(2, Nb))
+
+    Do i = 0, lx - 1
+      Do j = 0, ly - 1
+        Do k = 0, lz - 1
+          
+          spin_idx = 1 + i + j * lx + k * lx * ly
+
+          bound_idx = 1 + i + j * lx + k * lx * ly
+
+          bound_spin_ID(1, bound_idx) = spin_idx
+          bound_spin_ID(2, bound_idx) = 1 + mod(i + 1, lx) + j * lx + k * lx * ly
+
+          bound_spin_ID(1, bound_idx + lz * lx * ly) = spin_idx
+          bound_spin_ID(2, bound_idx + lz * lx * ly) = 1 + i + mod(j + 1, ly) * lx + k * lx * ly
+
+          bound_spin_ID(1, bound_idx + 2 * lz * lx * ly) = spin_idx
+          bound_spin_ID(2, bound_idx + 2 * lz * lx * ly) = 1 + i + j * lx + mod(k + 1, lz) * lx * ly
+
+        end do
+      end do
+    end do
+
+  else
+
+    print*, 'For a 1D lattice: lx > 0, ly==1 and lz==1'
+    print*, 'For a 2D lattice: lx > 0, ly > 0 and lz==1'
+    print*, 'For a 3D lattice: lx > 0, ly > 0 and lz > 0'
+    stop
   end if
 
 End Subroutine lattice
@@ -155,8 +235,33 @@ Subroutine init
     spin(i) = (-1)**(mod(i - 1, lx) + (i - 1) / lx)
   End do
 
-  Open(10, file='results_bin.dat')
-  Open(20, file='results_raw.dat')
+  print*,' '
+  write(*,'(1x,"Lattice: ", 2x,"lx=",I0,2x," ly=",I0,2x," lz=",I0)') lx, ly, lz
+  print*,' '
+  write(*,'(1x,"Number of spins: ",I0)') N
+  write(*,'(1x,"Number of bins: ",I0)') Nbins
+  write(*,'(1x,"Monte Carlo steps: ",ES7.1)') dble(mcsteps)
+  write(*,'(1x,"Termalization steps: ",ES7.1)') dble(term_steps)  
+  write(*,'(1x,"Temperatures: ",F6.4," -> ",F6.4)') 1.0d0 / beta, temp_ini + t_steps * dt
+
+  write(arq, '("bin_",I0,"x",I0,"x",I0,"_T=",F6.4,".dat")') lx, ly, lz, 1.0d0 / beta
+  Open(10, file=arq)
+
+  write(10, '(4x,"Energy",4x,"Specific Heat",4x,"Uniform Susceptibility", &
+           &  4x,"Staggered Magnetization",4x,"Number of H-operators",    &
+           &  4x,"(Number of H-operators)^2" )')
+  
+
+  write(arq, '("raw_",I0,"x",I0,"x",I0,"_T=",F6.4,".dat")') lx, ly, lz, 1.0d0 / beta
+  Open(20, file=arq)
+
+  write(20, '(4x,"Number of H-operators")')
+
+  write(arq, '("cut-off_",I0,"x",I0,"x",I0,"_T=",F6.4,".dat")') lx, ly, lz, 1.0d0 / beta
+  Open(30, file=arq)
+
+  write(30, '(4x,"Step",4x,"Cut-off",4x,"Number of H-operators", &
+           &  4x,"Maximum Number of H-operators" )')
 
 End Subroutine init
 
@@ -168,31 +273,39 @@ Subroutine diagonalupdate()
   Integer*4 :: p, bound_idx, op
   Real*8 :: ran
 
+  ! b(p) is the bound index. 
+  ! a(p) is the operator type, here 1 or 2.
+
   ! opstring = 2 * b(p) + a(p) - 1
-  ! Como 2 * b(p) sempre é um número inteiro par,
-  ! é o (a(p) - 1) que define qual tipo de operador temos.
-  ! Quando a(p) = 1, opstring vai ser necessariamente
-  ! par. Quando a(p) = 2, opstring é ímpar.
-  ! Os valores de a(p) e b(p) pode ser obtidos a partir
-  ! do opstring usando as equações,
-  ! a(p)=MOD(opstring[p],2)+1, e
-  ! b(p)=opstring[p]/2.
+
+  ! opstring == 0    => Identity operator.  
+  ! opstring == even => Diagonal operator.
+  ! opstring == odd  => Off-diagonal operator.
+
+  ! Since 2 * b(p) is always integer even, (a(p) - 1)
+  ! is the term which defines the operator type.
+  ! If a(p) = 1, opstring is even.
+  ! If a(p) = 2, opstring is odd.
+
+  ! From the opstring value is possible to get
+  ! bouth a(p) or b(p) using the relations,
+  ! a(p) = MOD(opstring[p], 2) + 1, and
+  ! b(p) = opstring[p] / 2.
 
   Do p = 0, L - 1
 
     op = opstring(p)
 
-    if (op == 0) then ! 0 == Operador Identidade
+    if (op == 0) then ! 0 == Identity operator.
 
-      ! Adiciona um operador diagonal,
-      ! se os spins da ligação não estão em
-      ! paralelo. 
+      ! Add a diagonal operator if the spins connected
+      ! by a bound, chosen at random, are anti-parallel.
 
       Call random_number(ran)
 
       bound_idx = int(ran * Nb) + 1
 
-      if (spin(bound(1, bound_idx)) /= spin(bound(2, bound_idx))) then
+      if (spin(bound_spin_ID(1, bound_idx)) /= spin(bound_spin_ID(2, bound_idx))) then
 
         Call random_number(ran)
 
@@ -204,9 +317,9 @@ Subroutine diagonalupdate()
 
       end if
 
-    elseif (mod(op, 2) == 0) then ! Par == operador diagonal
+    elseif (mod(op, 2) == 0) then ! Even == Diagonal operator.
 
-      ! Remove um operador diagonal,
+      ! Remove a diagonal operator.
       
       Call random_number(ran)
 
@@ -216,24 +329,19 @@ Subroutine diagonalupdate()
         NH = NH - 1
       end if
 
-    else !Ímpar == Operador fora da diagonal
+    else !Odd == Off-diagonal operator.
 
-      ! Aqui não mudamos o número de operadores,
-      ! só os spins são flipados.
-      ! Adicionar ou remover operadores fora da diagonal
-      ! é mais complicado, sendo necessário uma atenção
-      ! maior. 
-
-      ! Esse passo é permitido pq se existe um operador
-      ! fora diagonal em p=1, vai existir um outro operador
-      ! fora diagonal em p>1. Então é garantido que vamos flipar
-      ! dois operadores fora da diagonal. Se isso não acontecer
-      ! é pq estamos em uma configuração invalida para o sistema.
+      ! Change off-diagonal operators is more complex. Here,
+      ! we only make a basic flip of spins every time a off-diagonal
+      ! operator is found. This is allowed because a valid configuration
+      ! in this system always have with a zero or a odd number of 
+      ! off-diagonal operators. An even number of off-diagonal operators
+      ! breaks the boundary condition imposed by the trace \sum <\sigma|H|\sigma>. 
 
       bound_idx = op / 2
 
-      spin(bound(1, bound_idx)) = -spin(bound(1, bound_idx))
-      spin(bound(2, bound_idx)) = -spin(bound(2, bound_idx))
+      spin(bound_spin_ID(1, bound_idx)) = -spin(bound_spin_ID(1, bound_idx))
+      spin(bound_spin_ID(2, bound_idx)) = -spin(bound_spin_ID(2, bound_idx))
 
     end if
 
@@ -250,8 +358,6 @@ Subroutine linkvertices
   Integer*4 :: last_vertex1, last_vertex2
   Integer*4 :: spin_idx, spin_idx1, spin_idx2
 
-  !Trocar boun por spin_idx_of_bound
-
   first_vertex_visitted(:) = -1
   last_vertex_visitted(:) = -1
 
@@ -265,8 +371,8 @@ Subroutine linkvertices
        
        bound_idx = op / 2
        
-       spin_idx1 = bound(1, bound_idx)
-       spin_idx2 = bound(2, bound_idx)
+       spin_idx1 = bound_spin_ID(1, bound_idx)
+       spin_idx2 = bound_spin_ID(2, bound_idx)
        
        last_vertex1 = last_vertex_visitted(spin_idx1)
        last_vertex2 = last_vertex_visitted(spin_idx2)
@@ -327,12 +433,12 @@ Subroutine loopupdate
   Real*8 :: ran
   Integer*4 :: spin_idx, v0, vertex_in, vertex_out
 
-  ! -1 para vertex visitados porém não flipados.
-  ! -2 para vertex visitados e flipados.
+  ! -1 => Visited vertex but not flipped.
+  ! -2 => Visited and flipped vertex.
 
   Do v0 = 0, 4 * L - 1, 2
 
-    ! Se o vertex já foi visitado, pula.
+    ! If the vertex v0 was visited, cycle the loop.
     if (vertex_link(v0) < 0) cycle
 
     vertex_in = v0
@@ -343,20 +449,20 @@ Subroutine loopupdate
 
       do 
 
-        ! flip
+        ! Change the operator type.
         opstring(vertex_in / 4) = ieor(opstring(vertex_in / 4), 1)
         
-        ! marked as visitted
+        ! Mark as visitted
         vertex_link(vertex_in) = -2
         
-        ! next vertex
+        ! Next vertex
         vertex_out = ieor(vertex_in, 1)
 
         vertex_in = vertex_link(vertex_out)
 
         vertex_link(vertex_out) = -2
         
-        ! Se voltar onde começou o loop está completo.
+        ! If the loop is closed, exit.
         if (vertex_in == v0) exit
 
       end do
@@ -381,7 +487,7 @@ Subroutine loopupdate
 
   end do
 
-  ! flip das linhas de spins sem operadores.
+  ! Flip free spin lines. (Lines without operators)
 
   Do spin_idx = 1, N
 
@@ -413,35 +519,28 @@ Subroutine adjustcutoff(step)
 
   L_new = NH + NH / 3
 
-  ! Open(30, file='adjustcutoff.dat', position='append')
+  ! Uncomment for save data about the cut-off adjust.
 
-  ! if ( NH > NH_max) NH_max = NH
-
-  ! if (L_new <= L) then
-
-  !   write(30, *) step, L, NH, NH_max
-  !   return
-
-  ! else 
-
-  !   write(30, *) step, L_new, NH, NH_max
-
-  ! end if
+  ! Call cut_off_analisys(L_new, step)
 
   if (L_new <= L) return
 
-  Allocate(copy_opstring(0:L-1))
-  copy_opstring(:)=opstring(:)
+  Allocate(copy_opstring(0:L - 1))
+  
+  copy_opstring(:) = opstring(:)
 
   Deallocate(opstring)
-  Allocate(opstring(0:L_new-1))
-  opstring(0:L-1)=copy_opstring(:)
-  opstring(L:L_new-1)=0
+  Allocate(opstring(0:L_new - 1))
+  
+  opstring(0:L - 1) = copy_opstring(:)
+  opstring(L:L_new - 1) = 0
+  
   Deallocate(copy_opstring)
 
-  L=L_new
+  L = L_new
+  
   Deallocate (vertex_link)
-  Allocate(vertex_link(0:4*L-1))
+  Allocate(vertex_link(0:4 * L - 1))
 
 End Subroutine adjustcutoff
 
@@ -450,7 +549,48 @@ Subroutine measure
   Use Variables
   Implicit None
 
+
+  integer :: i, bound_idx, op, spin_idx1, spin_idx2, stag_mag
+  real(8) :: stag_mag2
+
+  stag_mag = 0
+
+  Do i = 1, N
+     stag_mag = stag_mag + spin(i) * (-1)**(mod(i - 1, lx) + (i - 1) / lx)
+  end do      
+
+  stag_mag = stag_mag / 2
+  stag_mag2 = 0.d0
+
+  Do i= 0, L - 1
+
+     op = opstring(i)
+
+     if (mod(op,2) == 1) then  
+
+        bound_idx = op / 2
+
+        spin_idx1 = bound_spin_ID(1, bound_idx)
+        spin_idx2 = bound_spin_ID(2, bound_idx)
+
+        spin(spin_idx1) = -spin(spin_idx1)
+        spin(spin_idx2) = -spin(spin_idx2)
+
+        stag_mag = stag_mag + 2 * spin(spin_idx1) * (-1)**(mod(spin_idx1 - 1, lx) + (spin_idx1 - 1) / lx)
+
+     end if
+
+     stag_mag2 = stag_mag2 + dfloat(stag_mag)**2
+
+  end do
+
+  stag_mag2 = stag_mag2 / dble(L)
+
+  staggered = staggered + stag_mag2
+  ususc = ususc + dble(sum(spin) / 2)**2
+
   n_opH = n_opH + dble(NH)
+  n_opH2 = n_opH2 + dble(NH)**2
 
   write(20, *) NH
 
@@ -461,13 +601,30 @@ Subroutine write_results
   Use Variables
   Implicit None
 
+  Real*8 :: Cv, ener
+
   n_opH = n_opH / dble(mcsteps)
+  n_opH2 = n_opH2 / dble(mcsteps)
+  ususc = ususc / dble(mcsteps)
+  staggered = staggered / dble(mcsteps)
 
   ener = - ( n_opH / (beta * N) - 0.25d0 * dble(Nb) / dble(N))
 
-  write(10, *) ener, n_opH
+  Cv = (n_opH2 - n_opH * (n_opH + 1.0d0)) / dble(N)
+
+  staggered = 3.d0 * staggered / dble(N)**2
+
+  ususc = beta * ususc / dble(N)
+
+  n_opH = n_opH / dble(N)
+  n_opH2 = n_opH2 / dble(N)
+
+  write(10, *) ener, Cv, ususc, staggered, n_opH, n_opH2
 
   n_opH = 0.0d0
+  n_opH2 = 0.0d0
+  ususc = 0.0d0
+  staggered = 0.0d0
 
 End Subroutine write_results
 
@@ -476,26 +633,54 @@ Subroutine results
   Use Variables
   Implicit None
 
-  Real*8 :: r1(Nbins)
+  Real*8 :: e(Nbins), c(Nbins), n_op(Nbins), n_op2(Nbins), x(Nbins), stag(Nbins)
   Integer*4 :: i
 
-  Open(10, file='ener.dat')
+  write(arq, '("bin_",I0,"x",I0,"x",I0,"_T=",F6.4,".dat")') lx, ly, lz, 1.0d0 / beta
+  Open(10, file=arq)
+
+  read(10, '(A)')
 
   Do i = 1, Nbins
 
-    read(10, *) r1(i)
+    read(10, *) e(i), c(i), x(i), stag(i), n_op(i), n_op2(i)
 
   end do 
 
   Close(10)
 
-  Open(20, file='results.dat', position='append')
+  e = e / dble(Nbins)
+  n_op = n_op / dble(Nbins)
+  n_op2 = n_op2 / dble(Nbins)
+  c = c / dble(Nbins)
+  x = x / dble(Nbins)
+  stag = stag / dble(Nbins)
 
-  write(20, *) beta, sum(r1) / dble(Nbins)
-
-  Close(20)
+  write(40, *) beta, sum(e), sum(c), sum(x), sum(stag), sum(n_op), sum(n_op2)
 
 End Subroutine results
+
+Subroutine cut_off_analisys(L_new, step)
+
+  Use Variables
+  Implicit None
+
+  Integer*4, intent(in) :: L_new, step
+
+  if ( NH > NH_max) NH_max = NH
+
+  if (L_new <= L) then
+
+    write(30, *) step, L, NH, NH_max
+    return
+
+  else 
+
+    write(30, *) step, L_new, NH, NH_max
+
+  end if
+
+End Subroutine cut_off_analisys
 
 Subroutine free_memory
 
@@ -504,7 +689,7 @@ Subroutine free_memory
 
   Deallocate(spin)
   Deallocate(opstring)
-  Deallocate(bound)
+  Deallocate(bound_spin_ID)
   Deallocate(first_vertex_visitted)
   Deallocate(last_vertex_visitted)
   Deallocate(vertex_link)
